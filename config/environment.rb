f401c7781851d706e732056d2f338e6b592b4ca3
Kierra M <k.merriman32@hotmail.com>
2019-10-06 22:10:41 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,10 @@ Bundler.require
 
 module Concerns
 end
-
+require_relative '../lib/concerns/findable'
 require_all 'lib'
+
+
+
+#require_relative '../lib/concerns/findable' 
+#any time you make a module you to require relative the file in enivornment so other classes can extend or include the module 
--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,4 +1,11 @@
 module Concerns::Findable 
   
-
+def find_by_name(name)
+    all.find {|song| song.name == name} #wont hard code
+ #@@all.find {|song| song.name == name} #will hard code
+  end 
+  
+  def  find_or_create_by_name(name)
+    find_by_name(name) || self.create(name)
+end
 end
